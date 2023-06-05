bcd18d8796430722855d12e95697ded545788a7a
AlanChou <choushenglun0414@gmail.com>
2023-06-05 11:25:40 +0800

    feat: initial commit

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,36 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      ["@babel/plugin-transform-flow-strip-types"],
+      ["@babel/plugin-proposal-decorators", { "legacy": true }],
+      ["@babel/plugin-proposal-class-properties", { "loose": true }],
+      ["module:react-native-dotenv", {
+        "envName": "APP_ENV",
+        "moduleName": "@env",
+        "path": ".env",
+      }],
+      ["module-resolver", {
+        "alias": {
+          "@Components": "./app/components",
+          "@Styles": "./assets/css",
+          "@Icon": "./assets/img",
+          "@Hooks": "./app/hooks",
+          "@Types": "./app/types",
+          "@Enum": "./app/enum",
+          "@Path": "./app/constants/path",
+          "@Utils": "./app/utils",
+          "@Screens": "./app/screens",
+          "@Navigation": "./app/navigation"
+        },
+        "extensions": [
+          ".js",
+          ".jsx",
+          ".ts",
+          ".tsx",
+        ]
+      }],
+    ]
   };
 };
